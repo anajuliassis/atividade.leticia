216b1ee875cbd7e7c5f39c192e1340576a8ea7b1
anajuliassis <anajuliamakely@gmail.com>
2025-03-18 21:39:44 -0300

    Update att.py

--- att.py
+++ att.py
@@ -1,7 +1,8 @@
 nome = input("nome: ")
 idade = input("idade: ")
-local = input("local: ")
 poder = input("poder: ")
 combate = input("combate: ")
 
-print(f"o herói {nome},tem {idade} anos, e domina o poder de {poder}, ela estava na {local} quando avistou {combate}, mas sem desespero, pois estava pronta pra salvar o mundo, e as pessoas que habitam ele.")
+print(f"o herói {nome},tem {idade} anos, e domina o poder da {poder} e estava pronto para enfrentar qualquer {combate} e salvar o mundo, e as pessoas que habitam ele.")
+
+print(f"Tudo começou quando um menino chamado {nome} foi atingido por um raio em seu laboratório e ganhou o poder {poder}. Ele protege a cidade da {combate}")  
